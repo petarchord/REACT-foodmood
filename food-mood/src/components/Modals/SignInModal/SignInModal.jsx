c632079a755269a331f08b6c456b77af6c53fd0a
petarchord <petarchord@hotmail.com>
2020-09-17 15:28:24 +0200

    sign in / sign up layout logic done

--- food-mood/src/components/Modals/SignInModal/SignInModal.jsx
+++ food-mood/src/components/Modals/SignInModal/SignInModal.jsx
@@ -9,12 +9,205 @@ import { GoMail } from "react-icons/go";
 
 Modal.setAppElement("#root");
 const SignInModal = ({ open, setModal }) => {
-  const [signUpClicked, setSignUpClicked] = useState(false);
+  const [currentLayout, setCurrentLayout] = useState(0);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const responseGoogle = (response) => {
     console.log(response);
     console.log(response.profileObj);
+  };
+
+  const renderCurrentLayout = () => {
+    switch (currentLayout) {
+      case 0: {
+        return (
+          <div className={styles.signin_wrapper}>
+            <div className={styles.header_exitbtn_wrapper}>
+              <h3>
+                Sign in to
+                <span style={{ color: "coral" }}> food mood</span>
+              </h3>
+              <MdClear
+                color="gray"
+                size="1.2em"
+                onClick={() => {
+                  setModal(false);
+                }}
+              />
+            </div>
+            <div className={styles.google_fb_wrapper}>
+              <GoogleLogin
+                clientId="37721926306-04lhbdhm6b4btbifcg2987jvcdfgec5r.apps.googleusercontent.com"
+                buttonText="Sign in with google"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={"single_host_origin"}
+                className={styles.google_btn}
+              />
+              <FacebookLogin
+                appId="778201083011422"
+                autoLoad={false}
+                fields="name,email,picture"
+                textButton="Sign in with facebook"
+                callback={responseFacebook}
+                cssClass={styles.fb_btn}
+                icon={
+                  <SiFacebook
+                    style={{
+                      width: "18px",
+                      height: "18px",
+                      marginRight: "15px",
+                    }}
+                    color="darkblue"
+                  />
+                }
+                // className={styles.fb_btn}
+              />
+            </div>
+            <p className={styles.or_divider}>or</p>
+            <button
+              className={styles.signin_btn}
+              onClick={() => {
+                setCurrentLayout(1);
+              }}
+            >
+              <GoMail
+                style={{ width: "18px", height: "18px", marginRight: "15px" }}
+              />
+              <span>Sign in with e-address</span>
+            </button>
+            <p className={styles.sign_up}>
+              You don't have an account?{" "}
+              <a
+                onClick={() => {
+                  setCurrentLayout(2);
+                }}
+              >
+                Sign up here.
+              </a>{" "}
+            </p>
+          </div>
+        );
+      }
+      case 1: {
+        return (
+          <div className={styles.signin_wrapper}>
+            <div className={styles.header_exitbtn_wrapper}>
+              <h3>
+                Sign in to
+                <span style={{ color: "coral" }}> food mood</span>
+              </h3>
+              <MdClear
+                color="gray"
+                size="1.2em"
+                onClick={() => {
+                  setModal(false);
+                }}
+              />
+            </div>
+            <form>
+              <div className={styles.email_field}>
+                <label htmlFor="email">E-address</label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email address"
+                  required
+                />
+              </div>
+              <div className={styles.password_field}>
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  required
+                />
+              </div>
+              <input
+                type="submit"
+                value="Sign in"
+                className={styles.submit_btn}
+              />
+            </form>
+            <p className={styles.sign_up}>
+              You don't have an account?{" "}
+              <a
+                onClick={() => {
+                  setCurrentLayout(2);
+                }}
+              >
+                Sign up here.
+              </a>{" "}
+            </p>
+            <p className={styles.sign_up}>
+              You want to sign in using facebook or google?
+              <a
+                onClick={() => {
+                  setCurrentLayout(0);
+                }}
+              >
+                Back
+              </a>
+            </p>
+          </div>
+        );
+      }
+      case 2: {
+        return (
+          <div className={styles.signin_wrapper}>
+            <div className={styles.header_exitbtn_wrapper}>
+              <h3>
+                Sign up to
+                <span style={{ color: "coral" }}> food mood</span>
+              </h3>
+              <MdClear
+                color="gray"
+                size="1.2em"
+                onClick={() => {
+                  setModal(false);
+                }}
+              />
+            </div>
+            <form>
+              <div className={styles.email_field}>
+                <label htmlFor="email">E-address</label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email address"
+                  required
+                />
+              </div>
+              <div className={styles.password_field}>
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  required
+                />
+              </div>
+              <input
+                type="submit"
+                value="Sign up"
+                className={styles.submit_btn}
+              />
+            </form>
+            <p className={styles.sign_up}>
+              You already have an account?
+              <a
+                onClick={() => {
+                  setCurrentLayout(0);
+                }}
+              >
+                Sign in here.
+              </a>
+            </p>
+          </div>
+        );
+      }
+    }
   };
 
   const responseFacebook = (response) => {
@@ -31,109 +224,7 @@ const SignInModal = ({ open, setModal }) => {
         className={styles.signin_modal_content}
         overlayClassName={styles.signin_modal_overlay}
       >
-        {/* <div className={styles.signup_wrapper}>
-        <div className={styles.header_exitbtn_wrapper}>
-            <h3>
-              Register to <span style={{ color: "coral" }}>food mood</span>
-            </h3>
-            <MdClear
-              color="gray"
-              size="1.2em"
-              onClick={() => {
-                setModal(false);
-              }}
-            />
-          </div>
-
-        </div> */}
-        <div className={styles.signin_wrapper}>
-          <div className={styles.header_exitbtn_wrapper}>
-            <h3>
-              {signUpClicked ? "Register to " : "Sign in to "}
-              <span style={{ color: "coral" }}>food mood</span>
-            </h3>
-            <MdClear
-              color="gray"
-              size="1.2em"
-              onClick={() => {
-                setModal(false);
-              }}
-            />
-          </div>
-          <div className={styles.google_fb_wrapper}>
-            <GoogleLogin
-              clientId="37721926306-04lhbdhm6b4btbifcg2987jvcdfgec5r.apps.googleusercontent.com"
-              buttonText="Sign in with google"
-              onSuccess={responseGoogle}
-              onFailure={responseGoogle}
-              cookiePolicy={"single_host_origin"}
-              className={styles.google_btn}
-            />
-            <FacebookLogin
-              appId="778201083011422"
-              autoLoad={false}
-              fields="name,email,picture"
-              textButton="Sign in with facebook"
-              callback={responseFacebook}
-              cssClass={styles.fb_btn}
-              icon={
-                <SiFacebook
-                  style={{ width: "18px", height: "18px", marginRight: "15px" }}
-                  color="darkblue"
-                />
-              }
-              // className={styles.fb_btn}
-            />
-          </div>
-          <p className={styles.or_divider}>or</p>
-
-          <button className={styles.signin_btn}>
-            <GoMail
-              style={{ width: "18px", height: "18px", marginRight: "15px" }}
-            />
-            <span>Sign in with e-address</span>
-          </button>
-          {/* <form>
-            <div className={styles.email_field}>
-              <label htmlFor="email">E-address</label>
-              <input
-                type="email"
-                name="email"
-                placeholder="Email address"
-                required
-              />
-            </div>
-            <div className={styles.password_field}>
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                name="password"
-                placeholder="Password"
-                required
-              />
-            </div>
-            <input
-              type="submit"
-              value="Sign in"
-              className={styles.submit_btn}
-            />
-          </form> */}
-          {/* <div>
-            <p></p>
-            <a href=""></a>
-          </div> */}
-          <p className={styles.sign_up}>
-            You don't have an account?{" "}
-            <a
-              onClick={() => {
-                Modal.scrollTo({ top: 0, behavior: "smooth" });
-                setSignUpClicked(true);
-              }}
-            >
-              Sign up here.
-            </a>{" "}
-          </p>
-        </div>
+        {renderCurrentLayout()}
       </Modal>
     </div>
   );
