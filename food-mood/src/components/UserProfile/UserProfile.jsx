e14eed7bb5a5b4b296e3f765cedcf7d5ffc22505
petarchord <petarchord@hotmail.com>
2020-10-09 11:45:15 +0200

    user profile small commit

--- food-mood/src/components/UserProfile/UserProfile.jsx
+++ food-mood/src/components/UserProfile/UserProfile.jsx
@@ -17,8 +17,10 @@ const UserProfile = () => {
         <h4>{user.name}</h4>
       </div>
       <div className={styles.user_info_form}>
+        <h5>Your personal info</h5>
+        <p>Don't forget to save your changes</p>
         <form>
-          <div className={styles.email_field}>
+          <div className={styles.name_field}>
             <label htmlFor="email">Name</label>
             <input type="text" name="name" value={user.name} />
           </div>
@@ -26,6 +28,15 @@ const UserProfile = () => {
             <label htmlFor="phone">Phone</label>
             <input type="text" name="phone" />
           </div>
+          <div className={styles.email_field}>
+                <label htmlFor="email">E-address</label>
+                <input
+                  type="email"
+                  name="email"
+                  value={user.email}
+                  disabled={true}
+                />
+              </div>
           <input type="submit" value="Save" className={styles.submit_btn} />
         </form>
       </div>
