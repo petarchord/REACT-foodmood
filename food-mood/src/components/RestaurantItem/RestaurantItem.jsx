4aa9ac81b7719d8123b4eb5b7616dd554e011b92
petarchord <petarchord@hotmail.com>
2020-10-31 00:36:23 +0100

    active class problem solving

--- food-mood/src/components/RestaurantItem/RestaurantItem.jsx
+++ food-mood/src/components/RestaurantItem/RestaurantItem.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React , { useState } from "react";
 import styles from "./RestaurantItem.module.scss";
+import cx from "classnames";
 
 const RestaurantItem = ({id,name,image}) => {
-    return ( <div className={styles.container}>
+    const [selected ,setSelected] = useState(0);
+    return ( <div className={cx(styles.container, selected === id ? styles.active : "")}
+    onClick = {() => {
+        setSelected(id);
+    }}
+    >
 <img src={image} alt={name}/>
 <h3>{name}</h3>
     </div> );
--- food-mood/src/components/Restaurants/Restaurants.jsx
+++ food-mood/src/components/Restaurants/Restaurants.jsx
@@ -5,19 +5,28 @@ import night_and_day_logo from "../../images/nightandday.png";
 import cezar_logo from "../../images/cezar-homepage-logo.png";
 
 
+
 const Restaurants = () => {
     return ( <div className={styles.container}>
 
         <RestaurantItem
+        id = {0}
         name="Night and Day"
         image = {night_and_day_logo}
         />
-        <RestaurantItem 
+        <RestaurantItem
+        id = {1} 
         name="Cezar"
         image = {cezar_logo}
         />
-        <RestaurantItem 
+        <RestaurantItem
+        id = {2} 
         name ="Go-go"
+        image={night_and_day_logo}
+        />
+        <RestaurantItem
+        id = {3} 
+        name ="Dva puta dva"
         image={night_and_day_logo}
         />
 
