eb434a7e3d707fdd0ef3092d44a03db06f779c18
petarchord <petarchord@hotmail.com>
2020-10-31 15:52:50 +0100

    minor styling changes in whole app

--- food-mood/src/components/RestaurantItem/RestaurantItem.jsx
+++ food-mood/src/components/RestaurantItem/RestaurantItem.jsx
@@ -2,9 +2,16 @@ import React , { useState } from "react";
 import styles from "./RestaurantItem.module.scss";
 import cx from "classnames";
 
-const RestaurantItem = ({id,name,image}) => {
-    const [selected ,setSelected] = useState(0);
-    return ( <div className={cx(styles.container, selected === id ? styles.active : "")}
+const RestaurantItem = ({id,name,image,selected,setSelected}) => {
+    // const [selected ,setSelected] = useState(0);
+
+   const toggleActiveStyles = index => {
+       if(selected === index)
+       return cx(styles.container,styles.active);
+       else
+       return cx(styles.container,styles.inactive)
+   }
+    return ( <div className={toggleActiveStyles(id)}
     onClick = {() => {
         setSelected(id);
     }}
--- food-mood/src/components/Restaurants/Restaurants.jsx
+++ food-mood/src/components/Restaurants/Restaurants.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./Restaurants.module.scss";
 import RestaurantItem from "../RestaurantItem/RestaurantItem";
 import night_and_day_logo from "../../images/nightandday.png";
@@ -7,27 +7,36 @@ import cezar_logo from "../../images/cezar-homepage-logo.png";
 
 
 const Restaurants = () => {
+    const [selected ,setSelected] = useState(0);
     return ( <div className={styles.container}>
 
         <RestaurantItem
         id = {0}
         name="Night and Day"
         image = {night_and_day_logo}
+        setSelected = {setSelected}
+        selected = {selected}
         />
         <RestaurantItem
         id = {1} 
         name="Cezar"
         image = {cezar_logo}
+        setSelected = {setSelected}
+        selected = {selected}
         />
         <RestaurantItem
         id = {2} 
         name ="Go-go"
         image={night_and_day_logo}
+        setSelected = {setSelected}
+        selected = {selected}
         />
         <RestaurantItem
         id = {3} 
         name ="Dva puta dva"
         image={night_and_day_logo}
+        setSelected = {setSelected}
+        selected = {selected}
         />
 
     </div> );
